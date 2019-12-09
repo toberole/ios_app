bcfe0fdf5dac8438cd00d9f2b8dd018efc820670
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-12-09 17:50:47 +0800

    note

--- ios_app/testui/Test1_ViewController.m
+++ ios_app/testui/Test1_ViewController.m
@@ -1,12 +1,22 @@
 #import "Test1_ViewController.h"
-/*通过拖拽 关联界面控件和控件的事件 */
+/*
+    通过拖拽 关联界面控件和控件的事件
+ 
+    动态添加View
+ */
 
 @interface Test1_ViewController ()
+
 @property (strong, nonatomic) IBOutlet UITextField *tf1;
 @property (strong, nonatomic) IBOutlet UITextField *tf2;
+
 @property (strong, nonatomic) IBOutlet UILabel *res_label;
 
+@property (strong, nonatomic) IBOutlet UIButton *btn_add;
+@property (strong, nonatomic) IBOutlet UIButton *btn_op;
+
 - (IBAction)add:(UIButton *)sender;
+- (IBAction)btn_op:(UIButton *)sender;
 
 @end
 
@@ -15,6 +25,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Test1_ViewController";
+    // [self addView_code];
+    [self addView_code1];
+}
+
+-(void)addView_code1{
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 300, 200, 50)];
+    [btn setTitle:@"xxxxxx" forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:@"x.png"] forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+}
+
+-(void)addView_code{
+    NSLog(@"--- addView_code ---");
+    UIButton *btn = [[UIButton alloc]init];
+    CGRect fream = CGRectMake(50, 100, 100, 200);
+    btn.frame = fream;
+    [btn setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+    [btn setTitle:@"动态添加按钮" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:1];
+    
+    [self.view addSubview:btn];
 }
 
 - (IBAction)add:(UIButton *)sender{
@@ -37,6 +68,80 @@
     
     // 关闭键盘 2
     [self.view endEditing:YES];
+}
+
+- (IBAction)btn_op:(UIButton *)sender {
+    /*
+         fream 可以修改位置和大小 修改大小中心位置变化
+         center 只能修改位置
+         bounds 只能修改大小 修改中心位置不变
+         transform 修改大小 位置 ...
+     
+        CGRect fream: x y 参考坐标是其父节点
+        CGRect bounds: x y 参考坐标是其本身 所以x和y总是0
+        CGPoint center: 参考坐标是其父节点
+     */
+    
+    // [self op1];
+    // [self op2];
+    // [self op3];
+     [self op4];
+    
+}
+
+#pragma maek 修改大小 transform动画
+-(void)op4{
+    // 启动动画
+    [UIView beginAnimations:nil context:nil];
+    // 设置动画时间
+    [UIView setAnimationDuration:2];
+    
+    //////////头尾式动画代码///////////
+//    CGRect bounds = _btn_add.bounds;
+//    bounds.size.height = bounds.size.height+10;
+//    bounds.size.width = bounds.size.width+10;
+//    _btn_add.bounds = bounds;
+    //////////动画代码///////////
+    // 提交动画
+    // [UIView commitAnimations];
+    
+    //////////Block 式动画代码///////////
+    [UIView animateWithDuration:2 animations:^{
+        CGRect bounds = _btn_add.bounds;
+        bounds.size.height = bounds.size.height+10;
+        bounds.size.width = bounds.size.width+10;
+        _btn_add.bounds = bounds;
+    }];
+    //////////动画代码///////////
+}
+
+#pragma maek 修改大小bounds
+-(void)op3{
+    CGRect bounds = _btn_add.bounds;
+    bounds.size.height = bounds.size.height+10;
+    bounds.size.width = bounds.size.width+10;
+    _btn_add.bounds = bounds;
+}
+
+#pragma maek 修改大小frame
+-(void)op2{
+    // 通过修改frame 修改大小
+    CGRect fream = _btn_add.frame;
+    fream.size.height = fream.size.height+10;
+    fream.size.width = fream.size.width+10;
+    _btn_add.frame = fream;
+}
+
+#pragma mark 移动frame
+-(void)op1{
+    // 通过修改frame 的x,y值来移动view
+    // 获取btn的原始的fram
+    CGRect fream =  _btn_add.frame;
+    // 上移动
+    fream.origin.y = fream.origin.y-10;
+    // 右移动
+    fream.origin.x = fream.origin.x+10;
+    _btn_add.frame = fream;
 }
 
 - (void)didReceiveMemoryWarning{
